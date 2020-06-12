a7e1c2718afbabdbc07ec8b385f911b26dab1de9
brendan-hall <brendan.j.hall@bath.edu>
2020-06-12 16:57:14 +0100

    Add ability to wait on multiple status codes

--- main/main.go
+++ main/main.go
@@ -5,19 +5,20 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
 
 func main() {
 	var url = flag.String("url", "http://localhost/", "URL to poll")
-	var responseCode = flag.Int("code", 200, "Response code to wait for")
+	var responseCode = flag.String("code", "200", "Response code to wait for")
 	var timeout = flag.Int("timeout", 2000, "Timeout before giving up in ms")
 	var interval = flag.Int("interval", 200, "Interval between polling in ms")
 	var localhost = flag.String("localhost", "", "Ip address to use for localhost")
 	flag.Parse()
 
-	fmt.Printf("Polling URL `%s` for response code %d for up to %d ms at %d ms intervals\n", *url, *responseCode, *timeout, *interval)
+	fmt.Printf("Polling URL `%s` for response code %s for up to %d ms at %d ms intervals\n", *url, *responseCode, *timeout, *interval)
 	startTime := time.Now()
 	timeoutDuration := time.Duration(*timeout) * time.Millisecond
 	sleepDuration := time.Duration(*interval) * time.Millisecond
@@ -27,7 +28,7 @@ func main() {
 	}
 	for {
 		res, err := http.Head(*url)
-		if err == nil && res.StatusCode == *responseCode {
+		if err == nil && containsStr(strings.Split(*responseCode, ","), strconv.Itoa(res.StatusCode)) {
 			fmt.Printf("Response header: %v", res)
 			os.Exit(0)
 		}
@@ -38,4 +39,13 @@ func main() {
 			os.Exit(1)
 		}
 	}
+}
+
+func containsStr(haystack []string, needle string) bool {
+	for _, straw := range haystack {
+		if needle == straw {
+			return true
+		}
+	}
+	return false
 }
